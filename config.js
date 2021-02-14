7ef52566176929b9c78455448030c75abcdb511b
m-rivera1 <mike.11.mxr@gmail.com>
2021-02-14 14:43:41 -0600

    fixed issue with API KEY

---
+++ config.js
@@ -0,0 +1,3 @@
+// API key 
+const API_KEY = "pk.eyJ1IjoibTFyaXZlcmEiLCJhIjoiY2trcmp6aWgwMWJ5azJubnZzOHZzMG82ZCJ9.lQJtnFLrUMfF3LWh8vokvQ";
+
--- logic.js
+++ logic.js
@@ -2,18 +2,6 @@
 //var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
  // "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
-var queryUrl = 	"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
-
-
-
-
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
-  console.log(data.features); 
-})
-
 // Create a map object
 var myMap = L.map("map", {
   center: [15.5994, -28.6731],
@@ -29,7 +17,29 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
   }).addTo(myMap)
 
+// Store json data in variable
+  var queryUrl = 	"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
+
+
+  // Perform a GET request to the query URL
+  d3.json(queryUrl, function(data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    //createFeatures(data.features);
+    console.log(data); 
+  })
   
+  // Add circles to map
+  L.circle(countries[i].location, {    
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: color,
+    // Adjust radius
+    radius: countries[i].points * 1500
+    }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
+  
+  
+
+
 
 
 
