019f63a38351f027336ea99462b1ed761c3d0208
m-rivera1 <mike.11.mxr@gmail.com>
2021-02-14 21:22:13 -0600

    functions added for tooltip, circles, and map generation

--- logic.js
+++ logic.js
@@ -14,54 +14,44 @@ function createFeatures(earthquakeData) { // *** earthquakeData is the DATA comi
   // Give each feature a popup describing the place and time of the earthquake
   function onEachFeaturePrep(feature, layer) { // **** GRAB only what is needed from the DATA *****
     layer.bindPopup("<h3>Magnitude:" + feature.properties.mag + "</h3><h3>Location: " + feature.properties.place +
-      "</h3><hr><p>When" + new Date(feature.properties.time) + "</p>");
+      "</h3><hr><p>When" + new Date(feature.properties.time) + "</p>")
+      console.log(feature.properties.mag,feature.properties.place,feature.properties.time);
   }
+  
+  function onEachFeaturePrep(feature, latlng) { // **** GRAB only what is needed from the DATA *****
+    L.circle(latlng), {    
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: "green",
+      // Adjust radius
+     radius: 2000
+      }//.addTo(myMap););
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, { // *** CALL the geoJSON function on the DATA
-    onEachFeature: onEachFeaturePrep
+    var earthquakes = L.geoJSON(earthquakeData, { // *** CALL the geoJSON function on the DATA
+        onEachFeature: onEachFeaturePrep
+
   });
 
+    // Sending our earthquakes layer to the createMap function
+    createMap(earthquakes);
 
+function makeMap(earthquakes) {
 
-//var earthquakeData = data.features
+  // Create a map object
+  var myMap = L.map("map", {
+    center: [37.09024, -95.712891],
+    zoom: 5,
+  });
 
-
-// Create a map object
-var myMap = L.map("map", {
-  center: [37.09024, -95.712891],
-  zoom: 5,
-});
-
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id:"mapbox/streets-v11",
-  accessToken: API_KEY
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id:"mapbox/streets-v11",
+    accessToken: API_KEY
   }).addTo(myMap)
 
-
-
-
-    
-    
-  
-  // Add circles to map
-  L.circle(earthquakeData.geometry), {    
-    fillOpacity: 0.75,
-    color: "white",
-    fillColor: "green",
-    // Adjust radius
-    radius: 2000
-    }//.addTo(myMap);
-  
-
-
-
-
-
-
-  }
+}}}
